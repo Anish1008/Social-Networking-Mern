df5bc07abfbde373a81d784be5f6aca70e476369
Anish <anishrockagarwal1024@gmail.com>
2023-09-17 22:48:42 +0530

    minor change

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(
 
 app.use(bodyParser.json());
 
-app.use(cors({origin: "https://social-media-r4cj.onrender.com"}));
+app.use(cors());
 app.options('*', cors())
 // Passport middleware
 app.use(passport.initialize());
